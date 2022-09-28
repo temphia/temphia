e08c42e17e82893fe60f4ad56c458dc2196b4d96
bornjre <bornjre@gmail.com>
2022-09-28 15:22:49 +0545

    tenant api impl

--- code/core/frontend/lib/apiv2/admin/tenant.ts
+++ code/core/frontend/lib/apiv2/admin/tenant.ts
@@ -1,14 +1,31 @@
-export {}
+import type { ApiBase } from "../base";
 
-/*
+export class TenantAPI {
+  base: ApiBase;
+  constructor(base: ApiBase) {
+    this.base = base;
+  }
 
-[GIN-debug] POST   /z/api/:tenant_id/v2/admin/tenant/ --> github.com/temphia/temphia/code/core/backend/app/server/middleware.(*Middleware).Authed.func1 (3 handlers)
-[GIN-debug] GET    /z/api/:tenant_id/v2/admin/tenant/ --> github.com/temphia/temphia/code/core/backend/app/server/middleware.(*Middleware).Authed.func1 (3 handlers)
-[GIN-debug] GET    /z/api/:tenant_id/v2/admin/tenant/domain --> github.com/temphia/temphia/code/core/backend/app/server/middleware.(*Middleware).Authed.func1 (3 handlers)
-[GIN-debug] POST   /z/api/:tenant_id/v2/admin/tenant/domain --> github.com/temphia/temphia/code/core/backend/app/server/middleware.(*Middleware).Authed.func1 (3 handlers)
-[GIN-debug] GET    /z/api/:tenant_id/v2/admin/tenant/domain/:id --> github.com/temphia/temphia/code/core/backend/app/server/middleware.(*Middleware).Authed.func1 (3 handlers)
-[GIN-debug] POST   /z/api/:tenant_id/v2/admin/tenant/domain/:id --> github.com/temphia/temphia/code/core/backend/app/server/middleware.(*Middleware).Authed.func1 (3 handlers)
-[GIN-debug] DELETE /z/api/:tenant_id/v2/admin/tenant/domain/:id --> github.com/temphia/temphia/code/core/backend/app/server/middleware.(*Middleware).Authed.func1 (3 handlers)
+  edit(data: any) {
+    return this.base.post("/admin/tenant/", data);
+  }
 
+  get() {
+    return this.base.get("/admin/tenant/");
+  }
 
-*/
+  get_domain() {
+    return this.base.get("/admin/tenant/domain");
+  }
+  new_domain(data: any) {
+    return this.base.post(`/admin/tenant/domain`, data);
+  }
+
+  edit_domain(did: string, data: any) {
+    return this.base.post(`/admin/tenant/domain/${did}`, data);
+  }
+
+  delete_domain(did: string) {
+    return this.base.delete(`/admin/tenant/domain/${did}`);
+  }
+}
