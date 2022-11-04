a011fe3d367bd55802287e93223fd85aad01a375
bornjre <bornjre@gmail.com>
2022-11-05 01:53:59 +0545

    more agent and plug impl

--- code/core/frontend/entries/portal/pages/admin/plug/agent/agent_new.svelte
+++ code/core/frontend/entries/portal/pages/admin/plug/agent/agent_new.svelte
@@ -1,1 +1,99 @@
-<div>Agent new</div>
+<script lang="ts">
+  import { getContext } from "svelte";
+  import { AutoForm, PortalService } from "../../core";
+  import { params } from "svelte-hash-router";
+
+  export let pid = $params.pid;
+
+  const app = getContext("__app__") as PortalService;
+  const api = app.api_manager.get_admin_plug_api();
+
+  let message = "";
+
+  const save = async (_data) => {
+    const resp = await api.new_agent(pid, _data);
+    if (!resp.ok) {
+      message = resp.data;
+      return;
+    }
+    app.nav.admin_agents(pid);
+  };
+</script>
+
+<AutoForm
+  {message}
+  schema={{
+    fields: [
+      {
+        name: "Id",
+        ftype: "TEXT_SLUG",
+        key_name: "id",
+      },
+      {
+        name: "Name",
+        ftype: "TEXT",
+        key_name: "name",
+      },
+      {
+        name: "Type",
+        ftype: "TEXT",
+        key_name: "type",
+      },
+      {
+        name: "Executor",
+        ftype: "TEXT",
+        key_name: "executor",
+      },
+      {
+        name: "Interface File",
+        ftype: "TEXT",
+        key_name: "iface_file",
+      },
+      {
+        name: "Entry File",
+        ftype: "TEXT",
+        key_name: "entry_file",
+      },
+      {
+        name: "Web Entry",
+        ftype: "TEXT",
+        key_name: "web_entry",
+      },
+      {
+        name: "Web Script",
+        ftype: "TEXT",
+        key_name: "web_script",
+      },
+      {
+        name: "Web Style",
+        ftype: "TEXT",
+        key_name: "web_style",
+      },
+      {
+        name: "Web Loader",
+        ftype: "TEXT",
+        key_name: "web_loader",
+      },
+      {
+        name: "Web Files",
+        ftype: "KEY_VALUE_TEXT",
+        key_name: "web_files",
+      },
+
+      {
+        name: "Env Vars",
+        ftype: "KEY_VALUE_TEXT",
+        key_name: "env_vars",
+      },
+      {
+        name: "Extra Meta",
+        ftype: "KEY_VALUE_TEXT",
+        key_name: "extra_meta",
+      },
+    ],
+    name: "New Agent",
+    required_fields: [],
+  }}
+  onSave={save}
+  data={{}}
+/>
--- code/core/frontend/entries/portal/pages/admin/plug/agent/agents.svelte
+++ code/core/frontend/entries/portal/pages/admin/plug/agent/agents.svelte
@@ -1,4 +1,4 @@
-<!-- <script lang="ts">
+<script lang="ts">
   import { getContext } from "svelte";
   import {
     AutoTable,
@@ -6,6 +6,9 @@
     FloatingAdd,
     PortalService,
   } from "../../core";
+  import { params } from "svelte-hash-router";
+
+  export let pid = $params.pid;
 
   let datas = [];
   let loading = true;
@@ -27,13 +30,23 @@
   // actions
 
   const action_execute = (id: string) =>
-    app.nav.admin_plug_dev_execute(id, "default");
+    app.nav.admin_plug_dev_execute(pid, id);
+  const action_agent_links = (id: string) => app.nav.admin_agent_links(pid, id);
+  const action_agent_exts = (id: string) => app.nav.admin_agent_ext(pid, id);
+  const action_agent_resources = (id: string) => {};
+  const action_dev_shell = (id: string) =>
+    app.nav.admin_plug_dev_shell(pid, id);
+  const action_dev_docs = (id: string) => app.nav.admin_plug_dev_docs(pid, id);
+  const action_delete = async (id: string) => {
+    const api = app.api_manager.get_admin_plug_api();
+    const resp = await api.delete_agent(pid, id);
+    if (!resp.ok) {
+      return;
+    }
+    load();
+  };
 
-
-
-  const action_new = () => {};
-
-
+  const action_new = () => app.nav.admin_agent_new(pid);
 </script>
 
 {#if loading}
@@ -41,12 +54,87 @@
 {:else}
   <AutoTable
     action_key="id"
-    actions={[]}
-    key_names={[]}
-    color={["executor"]}
+    actions={[
+      {
+        Name: "Execute",
+        Class: "bg-blue-400",
+
+        Action: action_execute,
+        icon: "lightning-bolt",
+      },
+      {
+        Name: "Edit",
+        Action: null, //async (aid) => app.navigator.goto_admin_agent_page(pid, aid),
+        drop: true,
+        icon: "pencil-alt",
+      },
+      {
+        Name: "Extern Execute",
+        Action: (agent_id) => {},
+        drop: true,
+        icon: "lightning-bolt",
+      },
+
+      {
+        Name: "Duplicate",
+        Action: (id) => {},
+        drop: true,
+        icon: "duplicate",
+      },
+
+      {
+        Name: "Docs",
+        drop: true,
+        icon: "book-open",
+        Action: action_dev_docs,
+      },
+
+      {
+        Name: "Links",
+        drop: true,
+        icon: "link",
+        Action: action_agent_links,
+      },
+
+      {
+        Name: "Extensions",
+        drop: true,
+        icon: "puzzle",
+        Action: action_agent_exts,
+      },
+
+      {
+        Name: "Resources",
+        drop: true,
+        icon: "paper-clip",
+        Action: action_agent_resources,
+      },
+
+      {
+        Name: "Dev Shell",
+        drop: true,
+        icon: "terminal",
+        Action: action_dev_shell,
+      },
+
+      {
+        Name: "Delete",
+        Action: action_delete,
+        drop: true,
+        icon: "trash",
+      },
+    ]}
+    key_names={[
+      ["id", "ID"],
+      ["name", "Name"],
+      ["type", "Type"],
+      ["executor", "Executor"],
+      ["plug_id", "Plug Id"],
+    ]}
     {datas}
     show_drop={true}
+    color={["type", "executor"]}
   />
 {/if}
 
-<FloatingAdd onClick={action_new} /> -->
+<FloatingAdd onClick={action_new} />
