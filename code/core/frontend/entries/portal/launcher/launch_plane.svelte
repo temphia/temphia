52923eeee282d19779887575cc06806040539801
bornjre <bornjre@gmail.com>
2022-11-07 03:35:35 +0545

    stash

--- code/core/frontend/entries/portal/launcher/launch_plane.svelte
+++ code/core/frontend/entries/portal/launcher/launch_plane.svelte
@@ -4,6 +4,30 @@
   export let store: Writable<boolean>;
 
   $: console.log("#STORE", $store);
+
+  // let display_state: Writable<"HIDDEN" | "FLOATING" | "NOT_FLOATING"> =
+  //   writable("HIDDEN");
 </script>
 
 <div style="display: {$store ? 'block' : 'none'};">Launch Plane</div>
+
+<!-- <style>
+  .hidden {
+    display: none;
+    width: 0px;
+    height: 0px;
+  }
+
+  .floating {
+    display: block;
+    position: fixed;
+    width: 90vw;
+    height: 90vh;
+  }
+
+  .not-floating {
+    display: block;
+    width: 90vw;
+    height: 90vh;
+  }
+</style> -->
