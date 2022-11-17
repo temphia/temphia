837193df9010d7f4599aae56bc8daa15d8a3f343
bornjre <bornjre@gmail.com>
2022-11-17 14:45:32 +0545

    iframe env and entry

---
+++ code/core/frontend/entries/portal/launcher/guestentry/iframe/iframe_pipe.ts
@@ -0,0 +1,43 @@
+import type {
+  Pipe,
+  PipeHandler,
+  PipeMessage,
+} from "../../../../../lib/engine/pipe";
+
+export class IFramePipe implements Pipe {
+  secret: string;
+  handlers: Set<PipeHandler>;
+  port: MessagePort;
+
+  constructor(secret: string, port: MessagePort) {
+    this.secret = secret;
+    this.handlers = new Set();
+    this.port = port;
+
+    window.addEventListener("message", (ev) => {
+      const decoded: PipeMessage = JSON.parse(ev.data);
+      this.handlers.forEach((fn) =>
+        fn(decoded.xid, decoded.action, decoded.data)
+      );
+    });
+  }
+
+  set_handler = (fn: PipeHandler) => {
+    this.handlers.add(fn);
+  };
+
+  remove_handler = (fn: PipeHandler) => {
+    this.handlers.delete(fn);
+  };
+
+  send = (xid: string, action: string, data: any) => {
+    const message = JSON.stringify({
+      xid,
+      data,
+      action,
+      parent_secret: this.secret,
+    });
+
+    this.port.postMessage(message);
+  };
+}
--- code/core/frontend/lib/altentry/sitejs/sitejs.ts
+++
@@ -1,2 +0,0 @@
-import { SiteUtils } from "../../../lib/utils/site";
-window["site_utils"] = new SiteUtils();
