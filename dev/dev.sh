29863b0871fa66451270efc3086ffcfd36a38e08
bornjre <bornjre@gmail.com>
2022-11-20 14:14:47 +0545

    dev helper script

---
+++ dev/dev.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+if [[ -f "go.mod" ]]
+then
+    cd dev
+fi
+
+
+tmux new "echo database; make backend_docker_run" ';'  \
+    split -h "echo server_backend; make backend_server_run" ';'  \
+    split "cd ../code/core/frontend && bash" ';' \
+    select-pane -L ';'  \
+    split "bash" ';' \
