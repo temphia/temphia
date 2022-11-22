672ed0afddb154c752be9549aa0077e3cd3c416d
bornjre <bornjre@gmail.com>
2022-11-22 16:08:46 +0545

    stash placeholder

--- code/core/frontend/entries/portal/pages/data/tableui/card/card.svelte
+++ code/core/frontend/entries/portal/pages/data/tableui/card/card.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import type { TableService } from "../../../../services/data/table";
-  import Renderer from "../core/renderer/renderer.svelte";
   import RendererLayout from "../core/renderer/layout.svelte";
+  import CardItem from "./card_item.svelte";
 
   export let table_service: TableService;
   export let show_editor;
@@ -15,6 +15,10 @@
   const data_store = table_service.state.data_store;
   const dirty_store = table_service.state.dirty_store;
   const nav_store = table_service.state.nav_store;
+
+  const rows = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
 </script>
 
 <RendererLayout
@@ -30,7 +34,13 @@
   on:on_change_to_grid
   layout={"card"}
 >
-  <div class="flex w-full" style="height:calc(100vh - 6em);">
-    <div>Card</div>
+  <div class="flex w-full" style="height:calc(100vh - 7em);">
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 h-full overflow-auto w-full p-2"
+    >
+      {#each rows as row}
+        <CardItem />
+      {/each}
+    </div>
   </div>
 </RendererLayout>
