3d0b6bc58aa1911f6610451e1f6e049ba7670a77
bornjre <bornjre@gmail.com>
2022-11-26 19:48:47 +0545

    view fixes and placeholder

--- code/core/frontend/entries/portal/pages/data/tableui/core/view/_inner.svelte
+++ code/core/frontend/entries/portal/pages/data/tableui/core/view/_inner.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { KvEditor } from "../../../../admin/core";
   import ViewItem from "./_view_item.svelte";
 
   export let columns;
@@ -19,8 +20,8 @@
   });
 </script>
 
-<div class="flex flex-col p-2 mt-2 shadow border">
-  <h2 class="inline-block text-xl text-slate-800">Name</h2>
+<div class="flex flex-col p-2 mt-2 border">
+  <h2 class="inline-block text-lg  text-slate-800">Name</h2>
   <div class="">
     <input
       type="text"
@@ -31,33 +32,21 @@
   </div>
 </div>
 
-<div class="flex flex-col border shadow text-slate-600">
-  <div class="flex justify-between p-1">
-    <h2 class="inline-block text-xl font-thin text-slate-800">
-      Filter Conditions
-    </h2>
-  </div>
+<div class="flex flex-col border text-slate-600">
+  <h2 class="inline-block text-lg  text-slate-800">Filter Conditions</h2>
 
   <div class="w-full p-2">
     <ViewItem {columns} bind:filter_conds />
   </div>
 </div>
 
-<div class="flex flex-col p-2 mt-2 shadow border">
-  <h2 class="inline-block text-xl text-slate-800">Search Text</h2>
-
-  <div class="">
-    <input
-      type="text"
-      bind:value={search_term}
-      placeholder="a word.."
-      class="w-full h-12 px-4 text-sm text-gray-700 bg-white border border-gray-300 rounded-lg duration-300 focus:border-teal-500 focus:outline-none focus:ring focus:ring-primary focus:ring-opacity-40"
-    />
-  </div>
+<div class="flex flex-col p-2 mt-2 border">
+  <h2 class="inline-block text-lg text-slate-800">View Tags</h2>
+  <KvEditor data={{}} />
 </div>
 
-<div class="flex flex-col p-2 mt-2 shadow border">
-  <h2 class="inline-block text-xl text-slate-800">Select Columns</h2>
+<div class="flex flex-col p-2 mt-2 border">
+  <h2 class="inline-block text-lg text-slate-800">Select Columns</h2>
 
   <div class="flex flex-wrap text-gray-700 gap-1">
     {#each columns as col}
@@ -69,17 +58,7 @@
   </div>
 </div>
 
-<div class="flex flex-col p-2 mt-2 shadow border">
-  <h2 class="inline-block text-xl text-slate-800">Main Column</h2>
-  <select class="p-2" bind:value={main_column}>
-    <option />
-    {#each columns as col}
-      <option value={col.slug}>{col.name}</option>
-    {/each}
-  </select>
-</div>
-
-<div class="flex flex-col p-2 mt-2 shadow border">
+<div class="flex flex-col p-2 mt-2 border">
   <h2 class="inline-block text-xl text-slate-800">Fetch Row Count</h2>
   <input type="number" class="border" bind:value={count} />
 </div>
