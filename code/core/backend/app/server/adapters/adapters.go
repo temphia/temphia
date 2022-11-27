72f809babfb6982b8ab612c24bd57b6f7d8ed571
bornjre <bornjre@gmail.com>
2022-11-27 13:03:23 +0545

    cmse

--- code/core/backend/app/server/adapters/adapters.go
+++ code/core/backend/app/server/adapters/adapters.go
@@ -2,14 +2,14 @@ package renderer
 
 import (
 	"github.com/temphia/temphia/code/core/backend/app/registry"
-	"github.com/temphia/temphia/code/core/backend/app/server/adapters/dynamic"
+	"github.com/temphia/temphia/code/core/backend/app/server/adapters/cmse"
 	"github.com/temphia/temphia/code/core/backend/app/server/adapters/landings"
 	"github.com/temphia/temphia/code/core/backend/app/server/adapters/launcher"
 	"github.com/temphia/temphia/code/core/backend/app/server/adapters/static"
 )
 
 func init() {
-	registry.G.SetAapterBuilder("dynamic", dynamic.New)
+	registry.G.SetAapterBuilder("cmse", cmse.New)
 	registry.G.SetAapterBuilder("landings", landings.New)
 	registry.G.SetAapterBuilder("launcher", launcher.New)
 	registry.G.SetAapterBuilder("static", static.New)
