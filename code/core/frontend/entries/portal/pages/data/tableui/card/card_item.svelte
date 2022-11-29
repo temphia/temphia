de208f926d2fd70bd51f7b91daa71d0cf2bcdfb7
bornjre <bornjre@gmail.com>
2022-11-30 03:24:48 +0545

    card layout fixes

--- code/core/frontend/entries/portal/pages/data/tableui/card/card_item.svelte
+++ code/core/frontend/entries/portal/pages/data/tableui/card/card_item.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Icon from "@krowten/svelte-heroicons/Icon.svelte";
   import type { Column } from "../../../../services/data";
   import Cicon from "../core/cicon/cicon.svelte";
   import type { Order } from "./order";
@@ -10,7 +11,7 @@
 
 <div class="relative mx-auto w-full">
   <div
-    class="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full"
+    class="relative inline-block duration-300 ease-in-out transition-transform transform hover:border border-blue-500 w-full"
   >
     <div class="shadow p-4 rounded-lg bg-white">
       <div class="flex justify-center relative rounded-lg overflow-hidden h-52">
@@ -24,17 +25,10 @@
           <div
             class="flex bg-white px-4 py-1 space-x-5 rounded-lg overflow-hidden shadow"
           >
-            <p class="flex items-center font-medium text-gray-800">
-              <svg
-                class="w-5 h-5 fill-current mr-2"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                ><path
-                  d="M480,226.15V80a48,48,0,0,0-48-48H80A48,48,0,0,0,32,80V226.15C13.74,231,0,246.89,0,266.67V472a8,8,0,0,0,8,8H24a8,8,0,0,0,8-8V416H480v56a8,8,0,0,0,8,8h16a8,8,0,0,0,8-8V266.67C512,246.89,498.26,231,480,226.15ZM64,192a32,32,0,0,1,32-32H208a32,32,0,0,1,32,32v32H64Zm384,32H272V192a32,32,0,0,1,32-32H416a32,32,0,0,1,32,32ZM80,64H432a16,16,0,0,1,16,16v56.9a63.27,63.27,0,0,0-32-8.9H304a63.9,63.9,0,0,0-48,21.71A63.9,63.9,0,0,0,208,128H96a63.27,63.27,0,0,0-32,8.9V80A16,16,0,0,1,80,64ZM32,384V266.67A10.69,10.69,0,0,1,42.67,256H469.33A10.69,10.69,0,0,1,480,266.67V384Z"
-                /></svg
-              >
-              3 + 1
-            </p>
+            <button class="inline-flex uppercase text-slate-600 text-sm">
+              <Icon name="pencil-alt" solid class="h-5 w-5" />
+              Edit
+            </button>
           </div>
         </div>
 
