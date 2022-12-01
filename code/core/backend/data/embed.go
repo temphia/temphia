bd1660bcd2fbaca51961c9eb996014c511634c7d
bornjre <bornjre@gmail.com>
2022-12-01 16:54:09 +0545

    fixes

--- code/core/backend/data/embed.go
+++ code/core/backend/data/embed.go
@@ -5,7 +5,11 @@ import (
 )
 
 //go:embed schema
+//go:embed schema/postgres
 //go:embed templates
 //go:embed repo
 //go:embed interfaces
+//go:embed assets/build
+//go:embed assets/lib
+//go:embed assets/static
 var dataDir embed.FS
