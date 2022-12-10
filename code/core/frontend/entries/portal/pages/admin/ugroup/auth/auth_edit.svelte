6860bab777f7c623ed1194221380650fae4d0f59
bornjre <bornjre@gmail.com>
2022-12-10 16:30:32 +0545

    auth impl

--- code/core/frontend/entries/portal/pages/admin/ugroup/auth/auth_edit.svelte
+++ code/core/frontend/entries/portal/pages/admin/ugroup/auth/auth_edit.svelte
@@ -1,1 +1,106 @@
-<div>Auths edit</div>
+<script lang="ts">
+  import { getContext } from "svelte";
+  import { AutoForm, LoadingSpinner, PortalService } from "../../core";
+  import { params } from "svelte-hash-router";
+
+  export let ugroup = $params.ugroup;
+  export let id = $params.id;
+
+  const app = getContext("__app__") as PortalService;
+  const api = app.api_manager.get_admin_ugroup_api();
+
+  let message = "";
+  let data = {};
+  let loading = true;
+
+  const load = async () => {
+    const resp = await api.get(ugroup);
+    if (!resp.ok) {
+      return;
+    }
+
+    data = resp.data;
+  };
+
+  load();
+
+  const save = async (_data) => {
+    const resp = await api.updateAuth(ugroup, id, _data);
+    if (!resp.ok) {
+      message = resp.data;
+      return;
+    }
+    app.nav.admin_ugroup_auths(ugroup);
+  };
+</script>
+
+{#if loading}
+  <LoadingSpinner />
+{:else}
+  <AutoForm
+    {message}
+    schema={{
+      fields: [
+        {
+          name: "Id",
+          ftype: "INT",
+          key_name: "id",
+          disabled: true,
+        },
+
+        {
+          name: "Name",
+          ftype: "TEXT",
+          key_name: "name",
+        },
+
+        {
+          name: "Type",
+          ftype: "TEXT",
+          key_name: "type",
+        },
+
+        {
+          name: "Provider",
+          ftype: "TEXT",
+          key_name: "provider",
+        },
+
+        {
+          name: "User Group",
+          ftype: "TEXT",
+          key_name: "user_group",
+          disabled: true,
+        },
+
+        {
+          name: "Scopes",
+          ftype: "MULTI_TEXT",
+          key_name: "scopes",
+        },
+
+        {
+          name: "New User If Not Exist",
+          ftype: "BOOL",
+          key_name: "newuser_ifnot_exists",
+        },
+
+        {
+          name: "Policy",
+          ftype: "TEXT_POLICY",
+          key_name: "policy",
+        },
+
+        {
+          name: "Extra Meta",
+          ftype: "KEY_VALUE_TEXT",
+          key_name: "extra_meta",
+        },
+      ],
+      name: "Edit User Group Auth",
+      required_fields: [],
+    }}
+    onSave={save}
+    {data}
+  />
+{/if}
--- code/core/frontend/entries/portal/pages/admin/ugroup/auth/auths.svelte
+++ code/core/frontend/entries/portal/pages/admin/ugroup/auth/auths.svelte
@@ -1,1 +1,72 @@
-<div>Auths</div>
+<script lang="ts">
+  import { getContext } from "svelte";
+  import {
+    AutoTable,
+    LoadingSpinner,
+    FloatingAdd,
+    PortalService,
+  } from "../../core";
+  import { params } from "svelte-hash-router";
+
+  export let ugroup = $params.ugroup;
+
+  const app = getContext("__app__") as PortalService;
+
+  let datas = [];
+  let loading = true;
+  const api = app.api_manager.get_admin_ugroup_api();
+
+  const load = async () => {
+    const resp = await api.listAuth(ugroup);
+    if (!resp.ok) {
+      return;
+    }
+
+    datas = resp.data;
+    loading = false;
+  };
+
+  load();
+
+  // actions
+
+  const action_edit = (id: string) =>
+    app.nav.admin_ugroup_auth_edit(ugroup, id);
+  const action_delete = async (id: string) => {
+    const resp = await api.deleteAuth(ugroup, id);
+    load();
+  };
+
+  const action_new = () => app.nav.admin_ugroup_auth_new(ugroup);
+</script>
+
+{#if loading}
+  <LoadingSpinner />
+{:else}
+  <AutoTable
+    action_key="slug"
+    actions={[
+      {
+        Name: "Edit",
+        Action: action_edit,
+        icon: "pencil-alt",
+      },
+      {
+        Name: "Delete",
+        Class: "bg-red-400",
+        Action: action_delete,
+        icon: "trash",
+      },
+    ]}
+    key_names={[
+      ["id", "Id"],
+      ["name", "Name"],
+      ["type", "Type"],
+      ["provider", "Provider"],
+    ]}
+    color={[]}
+    {datas}
+  />
+{/if}
+
+<FloatingAdd onClick={action_new} />
