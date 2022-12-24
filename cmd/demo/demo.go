08b8a6016f4cdad2ae6d7d04da01c4fbd58f0585
bornjre <bornjre@gmail.com>
2022-12-25 03:56:33 +0545

    placeholder

---
+++ cmd/demo/demo.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/k0kubun/pp"
+
+func main() {
+	pp.Println("Hello wold @ demo")
+}
---
+++ cmd/tests/tests.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/k0kubun/pp"
+
+func main() {
+	pp.Println("Hello world @ tests")
+
+}
