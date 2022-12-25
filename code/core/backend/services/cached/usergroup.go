42eaa57160e4da27828a8c9ca9c86cef65b08356
bornjre <bornjre@gmail.com>
2022-12-26 03:15:10 +0545

    placeholdet

---
+++ code/core/backend/services/cached/usergroup.go
@@ -0,0 +1,21 @@
+package cached
+
+import "github.com/temphia/temphia/code/core/backend/xtypes/models/entities"
+
+type UserGroupCache interface {
+	GetDatas() []*entities.UserGroupData
+	SetData(*entities.UserGroupData)
+
+	GetAuths() []*entities.UserGroupAuth
+	SetAuth(*entities.UserGroupAuth)
+
+	GetApps() []*entities.TargetApp
+	SetApp(*entities.TargetApp)
+
+	GetHooks() []*entities.TargetHook
+	SetHook(*entities.TargetHook)
+}
+
+type UserGroupService struct {
+	Cache UserGroupCache
+}
