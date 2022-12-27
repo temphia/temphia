e8c1a0f47a15678030c31108cab653ccea74dff1
bornjre <bornjre@gmail.com>
2022-12-28 02:51:27 +0545

    stash

--- code/core/backend/services/repohub/rprovider/github/github.go
+++ code/core/backend/services/repohub/rprovider/github/github.go
@@ -1,1 +1,29 @@
 package github
+
+/*
+import (
+	"github.com/temphia/temphia/code/core/backend/xtypes/models/entities"
+	"github.com/temphia/temphia/code/core/backend/xtypes/service/repox"
+)
+
+
+
+type Github struct {
+
+	// auth      bool
+	// authToken string
+}
+
+func (g *Github) Name() string { return "github" }
+
+func (g *Github) Query(tenantId string, opts *repox.RepoQuery) ([]entities.BPrint, error) {
+
+
+	return nil, nil
+}
+
+func (g *Github) GetItem(tenantid, group, slug string) (*entities.BPrint, error) { return nil, nil }
+func (g *Github) GetFile(tenantid, group, slug, file string) ([]byte, error)     { return nil, nil }
+func (g *Github) GetFileURL(tenantid, group, slug, file string) (string, error)  { return "", nil }
+
+*/
