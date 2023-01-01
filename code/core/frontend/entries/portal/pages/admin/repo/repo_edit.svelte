cf25b2001446fc6405d680fe2c3cfcd4922a1b11
bornjre <bornjre@gmail.com>
2023-01-01 12:49:05 +0545

    repo ui use autoform

--- code/core/frontend/entries/portal/pages/admin/repo/repo_edit.svelte
+++ code/core/frontend/entries/portal/pages/admin/repo/repo_edit.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import { getContext } from "svelte";
-  import { KvEditor } from "../../../../xcompo/common";
   import type { PortalService } from "../../../services";
   import { params } from "svelte-hash-router";
+  import { AutoForm, LoadingSpinner } from "../core";
 
   const app = getContext("__app__") as PortalService;
   const rapi = app.api_manager.get_admin_repo_api();
@@ -12,16 +12,7 @@
   let data = {};
   let message = "";
 
-  let get_meta_opts;
-  let meta_opts_modified = false;
-
-  let modified = false;
   let loading = true;
-
-  const set = (name) => (ev) => {
-    data = { ...data, [name]: ev.target.value };
-    modified = true;
-  };
 
   const load = async () => {
     const resp = await rapi.get(rid);
@@ -36,82 +27,58 @@
 
   load();
 
-  const saveHandle = async () => {
-    const fdata = { ...data };
-
-    if (meta_opts_modified) {
-      fdata["extra_meta"] = get_meta_opts();
+  const saveHandle = async (_data) => {
+    const resp = await rapi.update(rid, _data);
+    if (!resp.ok) {
+      console.log("Err", resp);
+      message = resp.data;
+      return;
     }
 
-    rapi.update(rid, fdata);
+    app.nav.admin_repos();
   };
 </script>
 
-<div class="h-full w-full overflow-auto">
-  <div class="md:p-12 bg-indigo-100 flex flex-row flex-wrap">
-    <div
-      class="md:w-1/2-screen m-0 p-5 bg-white w-full tw-h-full shadow md:rounded-lg"
-    >
-      <div class="text-2xl text-indigo-900">Edit Repo</div>
-      <p class="text-red-500">{message}</p>
+{#if loading}
+  <LoadingSpinner />
+{:else}
+  <AutoForm
+    {message}
+    schema={{
+      fields: [
+        {
+          name: "Id",
+          ftype: "TEXT_SLUG",
+          key_name: "id",
+        },
+        {
+          name: "Name",
+          ftype: "TEXT",
+          key_name: "name",
+        },
 
-      <div class="flex-col flex py-3">
-        <label class="pb-2 text-gray-700 font-semibold">Id</label>
-        <input
-          type="text"
-          disabled
-          value={data["id"]}
-          class="p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200"
-        />
-      </div>
+        {
+          name: "Provider",
+          ftype: "TEXT",
+          key_name: "provider",
+        },
 
-      <div class="flex-col flex py-3">
-        <label class="pb-2 text-gray-700 font-semibold">Name</label>
-        <input
-          type="text"
-          value={data["name"] || ""}
-          on:change={set("name")}
-          class="p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200"
-        />
-      </div>
+        {
+          name: "URL",
+          ftype: "TEXT",
+          key_name: "url",
+        },
 
-      <div class="flex-col flex py-3">
-        <label class="pb-2 text-gray-700 font-semibold">Provider</label>
-        <input
-          type="text"
-          value={data["provider"] || ""}
-          on:change={set("provider")}
-          class="p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200"
-        />
-      </div>
-
-      <div class="flex-col flex py-3">
-        <label class="pb-2 text-gray-700 font-semibold">URL</label>
-        <input
-          type="text"
-          value={data["url"] || ""}
-          on:change={set("url")}
-          class="p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200"
-        />
-      </div>
-
-      <div class="flex-col flex py-3 relative">
-        <label class="pb-2 text-gray-700 font-semibold">Extra Meta</label>
-        <KvEditor
-          data={data["extra_meta"] || {}}
-          bind:getData={get_meta_opts}
-          bind:modified={meta_opts_modified}
-        />
-      </div>
-
-      <div class="flex py-3">
-        {#if modified || meta_opts_modified}
-          <button
-            class="p-2 bg-blue-400 m-1 w-20 text-white rounded"
-            on:click={saveHandle}>Save</button
-          >
-        {/if}
-      </div>
-    </div>
-  </div>
-</div>
+        {
+          name: "Extra Meta",
+          ftype: "KEY_VALUE_TEXT",
+          key_name: "extra_meta",
+        },
+      ],
+      name: "Edit Repo",
+      required_fields: [],
+    }}
+    onSave={saveHandle}
+    {data}
+  />
+{/if}
--- code/core/frontend/entries/portal/pages/admin/repo/repo_new.svelte
+++ code/core/frontend/entries/portal/pages/admin/repo/repo_new.svelte
@@ -1,81 +1,55 @@
 <script lang="ts">
   import { getContext } from "svelte";
-  import { KvEditor } from "../../../../xcompo/common";
   import type { PortalService } from "../../../services";
+  import { AutoForm } from "../core";
 
   const app = getContext("__app__") as PortalService;
   const rapi = app.api_manager.get_admin_repo_api();
 
-  let name = "";
-  let url = "";
-  let provider = "";
-  let extra_meta = {};
-
   let message = "";
 
-  const saveHandle = async () => {
-    const resp = await rapi.new({
-      name,
-      url,
-      provider,
-      extra_meta,
-    });
+  const saveHandle = async (_data) => {
+    const resp = await rapi.new(_data);
     if (resp.status !== 200) {
       message = resp.data;
       return;
     }
 
-    // app.navigator.goto_admin_repo();
+    app.nav.admin_repos();
   };
 </script>
 
-<div class="h-full w-full overflow-auto">
-  <div class="md:p-12 bg-indigo-100 flex flex-row flex-wrap">
-    <div
-      class="md:w-1/2-screen m-0 p-5 bg-white w-full tw-h-full shadow md:rounded-lg"
-    >
-      <div class="text-2xl text-indigo-900">Add Repo</div>
+<AutoForm
+  {message}
+  schema={{
+    fields: [
+      {
+        name: "Name",
+        ftype: "TEXT",
+        key_name: "name",
+      },
 
-      <p class="text-red-500">{message}</p>
+      {
+        name: "Provider",
+        ftype: "TEXT",
+        key_name: "provider",
+      },
 
-      <div class="flex-col flex py-3">
-        <label class="pb-2 text-gray-700 font-semibold">Name</label>
-        <input
-          type="text"
-          bind:value={name}
-          class="p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200"
-        />
-      </div>
+      {
+        name: "URL",
+        ftype: "TEXT",
+        key_name: "url",
+      },
 
-      <div class="flex-col flex py-3">
-        <label class="pb-2 text-gray-700 font-semibold">Provider</label>
-        <input
-          type="text"
-          bind:value={provider}
-          class="p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200"
-        />
-      </div>
-
-      <div class="flex-col flex py-3">
-        <label class="pb-2 text-gray-700 font-semibold">URL</label>
-        <input
-          type="text"
-          bind:value={url}
-          class="p-2 shadow rounded-lg bg-gray-100 outline-none focus:bg-gray-200"
-        />
-      </div>
-
-      <div class="flex-col flex py-3 relative">
-        <label class="pb-2 text-gray-700 font-semibold">Extra Meta</label>
-        <KvEditor bind:data={extra_meta} />
-      </div>
-
-      <div class="flex py-3">
-        <button
-          on:click={saveHandle}
-          class="p-2 bg-blue-400 m-1 w-20 text-white rounded">Save</button
-        >
-      </div>
-    </div>
-  </div>
-</div>
+      {
+        name: "Extra Meta",
+        ftype: "KEY_VALUE_TEXT",
+        key_name: "extra_meta",
+      },
+    ],
+    name: "New Repo",
+    required_fields: [],
+  }}
+  onSave={saveHandle}
+  data={{}}
+/>
