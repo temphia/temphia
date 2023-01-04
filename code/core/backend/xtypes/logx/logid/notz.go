da3c003f6fb9735cb3d4e95037844be821fccd75
bornjre <bornjre@gmail.com>
2023-01-04 09:27:21 +0545

    notz logids

---
+++ code/core/backend/xtypes/logx/logid/notz.go
@@ -0,0 +1,12 @@
+package logid
+
+const (
+	NotzAdaptersBeforeInit     = "adapters_before_init"
+	NotzAdapterInitErr         = "adapter_init_err"
+	NotzAdapterInitOk          = "adapter_init_ok"
+	NotzAdaptersAfterInit      = "adapters_after_init"
+	NotzAdapterBuilderNotFound = "adapter_builder_not_found"
+	NotzAdapterBuildErr        = "adapter_build_err"
+	NotzAdapterBuildOk         = "adapter_build_ok"
+	NotzAdapterNotFound        = "adapter_instance_not_found"
+)
