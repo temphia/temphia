6249d5fb6740b9640c6cf453a2b1dc2db5adb6fb
bornjre <bornjre@gmail.com>
2023-01-05 21:53:33 +0545

    user tkt wip

--- code/core/backend/app/server/api_admin/user_tkt.go
+++ code/core/backend/app/server/api_admin/user_tkt.go
@@ -1,5 +1,34 @@
 package apiadmin
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/temphia/temphia/code/core/backend/xtypes/models/claim"
+)
 
-func (a *ApiAdmin) userTktAPI(rg *gin.RouterGroup) {}
+func (a *ApiAdmin) userTktAPI(rg *gin.RouterGroup) {
+	rg.GET("/", a.UgroupX(a.ugListUser))
+	rg.POST("/", a.UgroupX(a.ugAddUser))
+	rg.GET("/:user_id", a.UgroupX(a.ugGetUser))
+	rg.POST("/:user_id", a.UgroupX(a.ugUpdateUser))
+	rg.DELETE("/:user_id", a.UgroupX(a.ugDeleteUser))
+}
+
+func (a *ApiAdmin) ugListUser(ctx UgCtx)   {}
+func (a *ApiAdmin) ugAddUser(ctx UgCtx)    {}
+func (a *ApiAdmin) ugGetUser(ctx UgCtx)    {}
+func (a *ApiAdmin) ugUpdateUser(ctx UgCtx) {}
+func (a *ApiAdmin) ugDeleteUser(ctx UgCtx) {}
+
+type UgCtx struct {
+	uclaim *claim.UserMgmtTkt
+	http   *gin.Context
+}
+
+func (a *ApiAdmin) UgroupX(fn func(ctx UgCtx)) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		fn(UgCtx{
+			uclaim: nil,
+			http:   ctx,
+		})
+	}
+}
--- code/core/frontend/lib/apiv2/admin/user_tkt.ts
+++ code/core/frontend/lib/apiv2/admin/user_tkt.ts
@@ -5,4 +5,24 @@ export class AdminUserTktAPI {
   constructor(base: ApiBase) {
     this.base = base;
   }
+
+  list() {
+    return this.base.get("/admin/user_tkt/");
+  }
+
+  get(uid: string) {
+    return this.base.get(`/admin/user_tkt/${uid}`);
+  }
+
+  new(data: any) {
+    return this.base.post(`/admin/user_tkt/`, data);
+  }
+
+  update(uid: string, data: any) {
+    return this.base.post(`/admin/user_tkt/${uid}`, data);
+  }
+
+  delete(uid: string) {
+    return this.base.delete(`/admin/user_tkt/${uid}`);
+  }
 }
