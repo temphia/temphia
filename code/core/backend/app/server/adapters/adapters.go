c23a34c501262ad6075d572fc2293f9a522d68b6
bornjre <bornjre@gmail.com>
2023-01-05 12:26:18 +0545

    fixes

--- code/core/backend/app/server/adapters/adapters.go
+++ code/core/backend/app/server/adapters/adapters.go
@@ -3,7 +3,6 @@ package renderer
 import (
 	"github.com/temphia/temphia/code/core/backend/app/registry"
 	"github.com/temphia/temphia/code/core/backend/app/server/adapters/cmse"
-	"github.com/temphia/temphia/code/core/backend/app/server/adapters/landings"
 	"github.com/temphia/temphia/code/core/backend/app/server/adapters/launcher"
 	"github.com/temphia/temphia/code/core/backend/app/server/adapters/noop"
 	"github.com/temphia/temphia/code/core/backend/app/server/adapters/static"
@@ -13,7 +12,16 @@ func init() {
 	registry.G.SetAapterBuilder("cmse", cmse.New)
 	registry.G.SetAapterBuilder("noop", noop.New)
 	registry.G.SetAapterBuilder("", noop.New)
-	registry.G.SetAapterBuilder("landings", landings.New)
 	registry.G.SetAapterBuilder("launcher", launcher.New)
 	registry.G.SetAapterBuilder("static", static.New)
+
+	/*
+
+		templater
+		simple_blog
+		static_site
+
+
+	*/
+
 }
