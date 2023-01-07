11242982bff942b4934cb50a8cec66028649f9ce
bornjre <bornjre@gmail.com>
2023-01-07 20:22:32 +0545

    serve icon images

--- code/core/backend/app/server/assets.go
+++ code/core/backend/app/server/assets.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/temphia/temphia/code/core/backend/xtypes/httpx"
+	"github.com/temphia/temphia/code/core/backend/xtypes/store"
 )
 
 func (s *Server) assets(rg *gin.RouterGroup) {
@@ -12,7 +15,7 @@ func (s *Server) assets(rg *gin.RouterGroup) {
 	rg.GET("/build/:file", s.serveBuildAssets())
 	rg.GET("/lib/:file", s.serveLibAssets())
 	rg.GET("/public/:file", s.servePublicAssets())
-	rg.GET("/system/:otype/:file", s.serveSystemAssets())
+	rg.GET("/system/:tenant_id/:otype/:name", s.serveSystemAssets())
 }
 
 // static assets that are "static" not build files
@@ -36,20 +39,19 @@ func (s *Server) servePublicAssets() func(c *gin.Context) {
 }
 
 func (s *Server) serveSystemAssets() func(c *gin.Context) {
+
+	deps := s.app.GetDeps()
+	cab := deps.Cabinet().(store.CabinetHub)
+
 	return func(c *gin.Context) {
-		/*
+		csource := cab.Default(c.Param("tenant_id"))
+		out, err := csource.GetBlob(c.Request.Context(), "system", fmt.Sprintf("%s_%s.png", c.Param("otype"), c.Param("name")))
+		if err != nil {
+			c.Writer.WriteHeader(http.StatusNotFound)
+			return
+		}
 
-			ns_assets/icon_user_john11.png
-			ns_assets/icon_plug_xyz.png
-			ns_assets/icon_plug_agent_xyz_mno.png
-			ns_assets/icon_ugroup_super_admin.png
-			ns_assets/icon_tenant.png
-			ns_assets/authed_background.png
-
-
-
-		*/
-
+		httpx.WriteBinary(c, out)
 	}
 }
 
