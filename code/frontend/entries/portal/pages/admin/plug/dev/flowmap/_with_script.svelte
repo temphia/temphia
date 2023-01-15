50ae396dc0c593c2a38591a45928349df30e291a
bornjre <bornjre@gmail.com>
2023-01-16 03:33:18 +0545

    with script

---
+++ code/frontend/entries/portal/pages/admin/plug/dev/flowmap/_with_script.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let script: string;
+  export let key;
+
+  let loaded = !!window[key] || false;
+
+  const onLoad = (ev) => {
+    loaded = true;
+  };
+</script>
+
+<svelte:head>
+  <script on:load={onLoad} src={loaded ? "" : script}></script>
+</svelte:head>
+
+{#if loaded}
+  <slot />
+{/if}
