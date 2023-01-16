5a0b0efc1fc3c370f915a945bba67b78cb4064d2
bornjre <bornjre@gmail.com>
2023-01-16 20:49:42 +0545

    stash

--- code/frontend/entries/portal/pages/admin/resource/datagroup/datagroup_new.svelte
+++ code/frontend/entries/portal/pages/admin/resource/datagroup/datagroup_new.svelte
@@ -1,1 +1,9 @@
-<div>Datagroup New</div>
+<script lang="ts">
+  import { getContext } from "svelte";
+  import type { PortalService } from "../../core";
+
+  const app = getContext("__app__") as PortalService;
+  const api = app.api_manager.get_admin_resource_api();
+</script>
+
+<div>Datagroup Edit</div>
