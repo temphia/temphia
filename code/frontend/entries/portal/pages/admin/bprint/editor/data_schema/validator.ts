d10c0a0158f128f4b643cac8f4bc908e11066797
bornjre <bornjre@gmail.com>
2023-01-19 01:44:06 +0545

    validator fix

--- code/frontend/entries/portal/pages/admin/bprint/editor/data_schema/validator.ts
+++ code/frontend/entries/portal/pages/admin/bprint/editor/data_schema/validator.ts
@@ -2,11 +2,15 @@ import { CtypeIcons } from "../../../../data/tableui/core/fields/field";
 import type { Column, ColumnRef, State, Table } from "./builder";
 
 export const Validate = (group: State): string => {
+  console.log("@validator", group);
+
   if (group.slug.length > 20) {
     return "error group slug is bigger than 20";
   }
 
   for (var i = 0; i < group.tables.length; i++) {
+    console.log("@table", i);
+
     const tbl = group.tables[i];
 
     if (tbl.slug.length > 20) {
@@ -14,6 +18,8 @@ export const Validate = (group: State): string => {
     }
 
     for (var j = 0; j < tbl.columns.length; j++) {
+      console.log("@column", j);
+
       const col = tbl.columns[j];
 
       if (col.slug.length > 20) {
@@ -21,10 +27,8 @@ export const Validate = (group: State): string => {
       }
 
       if (!CtypeIcons[col.ctype]) {
-        `error unknown column type [${tbl.name} / ${col.name}] : ${col.ctype}`;
+        return `error unknown column type [${tbl.name} / ${col.name}] : ${col.ctype}`;
       }
-
-      col.ctype;
     }
   }
 
