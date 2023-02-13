5aed85a0efd6c3b24534c40ff9e7443a4e804852
bornjre <bornjre@gmail.com>
2023-02-13 13:13:24 +0545

    form placeholder

--- code/backend/xtypes/service/repox/xbprint/bundle.go
+++ code/backend/xtypes/service/repox/xbprint/bundle.go
@@ -1,12 +1,20 @@
 package xbprint
 
 type Bundle struct {
-	Type  string       `json:"type,omitempty"`
-	Items []BundleItem `json:"items,omitempty"`
+	Type      string       `json:"type,omitempty"`
+	Items     []BundleItem `json:"items,omitempty"`
+	FormItems []FormItem   `json:"form_items,omitempty"`
 }
 
 type BundleItem struct {
-	Name string `json:"name,omitempty"`
-	Type string `json:"type,omitempty"`
-	File string `json:"file,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Type      string `json:"type,omitempty"`
+	File      string `json:"file,omitempty"`
+	Templated bool   `json:"templated,omitempty"`
+}
+
+type FormItem struct {
+	Name  string `json:"name,omitempty"`
+	Type  string `json:"type,omitempty"`
+	Value string `json:"value,omitempty"`
 }
