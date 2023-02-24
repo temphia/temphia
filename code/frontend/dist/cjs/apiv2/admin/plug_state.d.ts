1d320dee8009e49222ae8682a0eb94f71e8cecbb
bornjre <bornjre@gmail.com>
2023-02-25 05:19:39 +0545

    update generated code

--- code/frontend/dist/cjs/apiv2/admin/plug_state.d.ts
+++ code/frontend/dist/cjs/apiv2/admin/plug_state.d.ts
@@ -2,11 +2,7 @@ import type { ApiBase } from "../base";
 export declare class AdminPlugStateTktAPI {
     base: ApiBase;
     constructor(base: ApiBase);
-    list(qparms: {
-        key_cursor?: string;
-        page?: number;
-        page_count?: number;
-    }): Promise<import("../http").HttpResponse>;
+    query(options: any): Promise<import("../http").HttpResponse>;
     add(key: string, value: string): Promise<import("../http").HttpResponse>;
     update(key: string, value: string): Promise<import("../http").HttpResponse>;
     delete(key: string): Promise<import("../http").HttpResponse>;
--- code/frontend/dist/cjs/apiv2/admin/plug_state.js
+++ code/frontend/dist/cjs/apiv2/admin/plug_state.js
@@ -5,32 +5,28 @@ class AdminPlugStateTktAPI {
     constructor(base) {
         this.base = base;
     }
-    list(qparms) {
-        const u = new URLSearchParams();
-        u.set("page", String(qparms.page || 0));
-        u.set("page_count", String(qparms.page_count || 0));
-        u.set("key_cursor", qparms.key_cursor || "");
-        return this.base.get(`/admin/plug_state/`);
+    query(options) {
+        return this.base.post(`/admin/plug_state/query`, options);
     }
     add(key, value) {
-        return this.base.post(`/admin/plug_state/`, {
+        return this.base.post(`/admin/plug_state/key`, {
             key,
             value,
         });
     }
     update(key, value) {
-        return this.base.post(`/admin/plug_state/${key}`, {
+        return this.base.post(`/admin/plug_state/key/${key}`, {
             key,
             value,
         });
     }
     delete(key) {
-        return this.base.delete(`/admin/plug_state/${key}`, {
+        return this.base.delete(`/admin/plug_state/key/${key}`, {
             key,
         });
     }
     get(key) {
-        return this.base.delete(`/admin/plug_state/${key}`);
+        return this.base.delete(`/admin/plug_state/key/${key}`);
     }
 }
 exports.AdminPlugStateTktAPI = AdminPlugStateTktAPI;
