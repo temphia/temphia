a1412435da342544d6d0f2434cb65394b863bbf9
bornjre <bornjre@gmail.com>
2023-03-13 19:21:09 +0545

    refactor bindings wip

--- code/backend/engine/binders/standard/binder.go
+++ code/backend/engine/binders/standard/binder.go
@@ -107,6 +107,17 @@ func (b *Binder) Execute() (*event.Response, error) {
 	return eresp, nil
 }
 
+// bindings
+
+func (b *Binder) PlugKVBindingsGet() bindx.PlugKV { return &b.plugKV }
+func (b *Binder) SelfBindingsGet() bindx.Self     { return &b.self }
+func (b *Binder) NetGet() bindx.Net               { return &b.net }
+
+func (b *Binder) SockdBindingsGet() bindx.Sockd     { return &b.sockd }
+func (b *Binder) CabinetBindingsGet() bindx.Cabinet { return &b.cabinet }
+func (b *Binder) NodeCacheGet() bindx.NodeCache     { return &b.ncache }
+func (b *Binder) UserBindingsGet() bindx.User       { return &b.user }
+
 // private
 
 func (b *Binder) logInfo() *zerolog.Event {
