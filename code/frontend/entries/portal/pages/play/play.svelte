e60e5709cc628a55e61ca5f07fb5d2b668f3cfb4
bornjre <bornjre@gmail.com>
2023-03-15 03:25:09 +0545

    create stepper component

--- code/frontend/entries/portal/pages/play/play.svelte
+++ code/frontend/entries/portal/pages/play/play.svelte
@@ -3,6 +3,8 @@
   import type { PortalService } from "../../services";
 
   import LiveDev from "./livedev/index.svelte";
+
+  import StepperExample from "./stepper_example.svelte";
 </script>
 
-<LiveDev />
+<StepperExample />
