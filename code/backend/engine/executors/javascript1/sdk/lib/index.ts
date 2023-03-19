1c914049dd3d3376f20a197d6eac91ff4d31c8dd
bornjre <bornjre@gmail.com>
2023-03-19 19:02:54 +0545

    export self bindings

--- code/backend/engine/executors/javascript1/sdk/lib/index.ts
+++ code/backend/engine/executors/javascript1/sdk/lib/index.ts
@@ -4,10 +4,12 @@ import { Request, Response } from "./http";
 import { CabFolder } from "./cabinet";
 import { SockdRoom } from "./sockd";
 import { utils} from "./utils"
+import {self} from "./self"
 
 
 export {
   core,
+  self,
   utils,
   plugkv,
   Request,
--- code/backend/engine/executors/javascript1/sdk/lib/self.ts
+++ code/backend/engine/executors/javascript1/sdk/lib/self.ts
@@ -1,7 +1,29 @@
-declare function _self_list_resource(): [any, string]
-declare function _self_get_resource(name: string): [any, string]
-declare function _self_inlinks(): [any, string]
-declare function _self_outlinks(): [any, string]
-declare function _self_module_execute(name: string, method: string, path: string, data: any): [any, string]
-declare function _self_link_execute(name: string, method: string, path: string, data: any, async: boolean, detached: boolean): [any, string]
-declare function _self_fork_execute(method: string, data: any):  string
+declare function _self_list_resource(): [any, string];
+declare function _self_get_resource(name: string): [any, string];
+declare function _self_inlinks(): [any, string];
+declare function _self_outlinks(): [any, string];
+declare function _self_module_execute(
+  name: string,
+  method: string,
+  path: string,
+  data: any
+): [any, string];
+declare function _self_link_execute(
+  name: string,
+  method: string,
+  path: string,
+  data: any,
+  async: boolean,
+  detached: boolean
+): [any, string];
+declare function _self_fork_execute(method: string, data: any): string;
+
+export const self = {
+  list_resource: _self_list_resource,
+  get_resource: _self_get_resource,
+  inlinks: _self_inlinks,
+  outlinks: _self_outlinks,
+  module_execute: _self_module_execute,
+  link_execute: _self_link_execute,
+  fork_execute: _self_fork_execute,
+};
