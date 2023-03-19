8a13b3c0756468e65a468254e56bdd526ebe4584
bornjre <bornjre@gmail.com>
2023-03-19 23:24:29 +0545

    nit

--- code/frontend/entries/portal/pages/play/play.svelte
+++ code/frontend/entries/portal/pages/play/play.svelte
@@ -2,9 +2,8 @@
   import { getContext } from "svelte";
   import type { PortalService } from "../../services";
 
-  import LiveDev from "../admin/plug/dev/liveshell/index.svelte";
 
   import StepperExample from "./stepper_example.svelte";
 </script>
 
-<LiveDev />
+<StepperExample />
