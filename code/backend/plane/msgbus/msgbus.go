6e64e62a4cfad38bac749aec29145ddfbd3e9522
bornjre <bornjre@gmail.com>
2023-03-22 11:59:30 +0545

    remove old code

--- code/backend/plane/msgbus/msgbus.go
+++ code/backend/plane/msgbus/msgbus.go
@@ -1,62 +1,60 @@
 package msgbus
 
-import "sync"
+import (
+	"sync"
 
-var (
-	_ IMsgBus = (*MessageBus)(nil)
+	"github.com/temphia/temphia/code/backend/xtypes/models/entities"
+	"github.com/temphia/temphia/code/backend/xtypes/store"
+	"github.com/temphia/temphia/code/backend/xtypes/xplane"
 )
 
-type MessageBus struct {
-	globalTopics BusRoom
-	tenantTopics map[string]*BusRoom
-	ttLock       sync.RWMutex
+type MsgBus struct {
+	store   SubStore
+	db      store.SystemOps
+	nodeId  int64
+	generic bool
 }
 
-func New() *MessageBus {
-	return &MessageBus{
-		globalTopics: BusRoom{
-			topics: make(map[string][]Subscription),
-			mlock:  sync.RWMutex{},
-		},
-		tenantTopics: make(map[string]*BusRoom),
-		ttLock:       sync.RWMutex{},
-	}
-}
-
-func (mb *MessageBus) Subscribe(topic string, callback func(msg Message)) {
-	mb.globalTopics.AddSub(topic, callback)
-}
-
-func (mb *MessageBus) Submit(topic string, msg Message) {
-	mb.globalTopics.SendSub(topic, msg)
-}
-
-func (mb *MessageBus) SubscribeTenant(tenant, topic string, callback func(msg Message)) {
-
-	mb.ttLock.Lock()
-	troom := mb.tenantTopics[tenant]
-	if troom == nil {
-		troom = &BusRoom{
-			topics:     make(map[string][]Subscription),
-			mlock:      sync.RWMutex{},
+func New(nodeId int64, db store.SystemOps) *MsgBus {
+	return &MsgBus{
+		store: SubStore{
+			topics:     make(map[string][]xplane.Subscription),
+			mlock:      sync.Mutex{},
 			subCounter: 0,
-		}
-		mb.tenantTopics[tenant] = troom
+		},
+		db:      db,
+		nodeId:  nodeId,
+		generic: true,
 	}
-	mb.ttLock.Unlock()
-
-	troom.AddSub(topic, callback)
 }
 
-func (mb *MessageBus) SubmitTenant(tenant, topic string, msg Message) {
+func (m *MsgBus) Run() error {
 
-	mb.ttLock.RLock()
-	troom := mb.tenantTopics[tenant]
-	if troom == nil {
-		mb.ttLock.RUnlock()
-		return
+	currMax, err := m.getMaxId()
+	if err != nil {
+		return err
 	}
-	mb.ttLock.RUnlock()
 
-	troom.SendSub(topic, msg)
+	if m.generic {
+		return m.genericPoll(currMax)
+	}
+	return m.watchPoll(currMax)
+}
+
+func (m *MsgBus) Submit(topic string, msg xplane.Message) (int64, error) {
+	return 0, m.db.AddSystemEvent(&entities.SystemEvent{
+		Type:      topic,
+		Data:      msg.Data,
+		ExtraMeta: entities.JsonStrMap{},
+		TenantId:  msg.Tenant,
+	})
+}
+
+func (m *MsgBus) Subscribe(topic string, ch chan xplane.Message) (int32, error) {
+	return m.store.addSub(topic, ch), nil
+}
+
+func (m *MsgBus) UnSubscribe(topic string, subid int32) error {
+	m.store.removeSub(topic, subid)
+	return nil
 }
