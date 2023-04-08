efe5171e704f5e8e48bf3a4a1bc9fc57d0e0ae45
bornjre <bornjre@gmail.com>
2023-04-09 05:19:05 +0545

    sqlite add hook

--- code/backend/stores/upper/vendors/sqlite/sqlite.go
+++ code/backend/stores/upper/vendors/sqlite/sqlite.go
@@ -3,6 +3,8 @@ package sqlite
 import (
 	"database/sql"
 
+	"github.com/k0kubun/pp"
+	"github.com/mattn/go-sqlite3"
 	"github.com/temphia/temphia/code/backend/app/config"
 	"github.com/temphia/temphia/code/backend/app/registry"
 	"github.com/temphia/temphia/code/backend/libx/dbutils"
@@ -21,11 +23,27 @@ func init() {
 type sl struct{}
 
 func (sl) Db(so *config.StoreSource) (db.Session, error) {
+
 	var settings = sqlite.ConnectionURL{
 		Database: so.HostPath,
 	}
 
-	return sqlite.Open(settings)
+	dvr := &sqlite3.SQLiteDriver{
+		Extensions: []string{},
+		ConnectHook: func(sc *sqlite3.SQLiteConn) error {
+			pp.Println("@connect")
+			return nil
+		},
+	}
+
+	sql.Register("sqlite3_temphia", dvr)
+
+	db, err := sql.Open("sqlite3_temphia", settings.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return sqlite.New(db)
 }
 
 func (sl) NewTx(sqlTx *sql.Tx) (dbutils.Tx, error) {
