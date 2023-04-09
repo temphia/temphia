93c0716bbe39199bd01682923320c805b8ea28d4
bornjre <bornjre@gmail.com>
2023-04-09 06:56:49 +0545

    sqlite audit log wip

--- cmd/play/play.go
+++ cmd/play/play.go
@@ -1,65 +1,84 @@
 package main
 
 import (
-	"github.com/doug-martin/goqu/v9"
-	"github.com/k0kubun/pp"
-	"github.com/temphia/temphia/code/backend/stores/upper/dyndb/filter"
-	"github.com/temphia/temphia/code/backend/xtypes/store/dyndb"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	// Connect to SQLite database
 
-	pp.Println(goqu.From("test").Where(goqu.Ex{
-		"d": []string{"a", "b", "c"},
-	}).ToSQL())
+	const memURL = "file::memory:?mode=memory&cache=shared"
 
-	pp.Println("@t")
+	db, err := sql.Open("sqlite3", memURL)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	transform([][]dyndb.FilterCond{
-		{
-			dyndb.FilterCond{
-				Column: "aaa",
-				Cond:   "eq",
-				Value:  "111",
-			},
+	// Create table for testing
+	_, err = db.Exec(`
+		CREATE TABLE tablexyz (
+			__id INTEGER,
+			__mod_ctx TEXT,
+			__version INTEGER,
+			data TEXT
+		);
 
-			dyndb.FilterCond{
-				Column: "bb",
-				Cond:   "eq",
-				Value:  "111",
-			},
-		},
+		CREATE TABLE tablexyz_log (
+			id INTEGER,
+			row_id INTEGER,
+			mod_sig TEXT,
+			user_id TEXT,
+			type TEXT,
+			row_version INTEGER,
+			data TEXT
+		)
 
-		{
-			dyndb.FilterCond{
-				Column: "ccc",
-				Cond:   "eq",
-				Value:  "43333",
-			},
-		},
-	})
 
-}
-
-func transform(filters [][]dyndb.FilterCond) {
-
-	exprs := make([]goqu.Expression, 0)
-
-	for _, qry := range filters {
-		inner := goqu.ExOr{}
-
-		for _, fc := range qry {
-
-			switch fc.Cond {
-			case filter.OptrGT:
-				inner[fc.Column] = goqu.Op{"gt": fc.Value}
-			default:
-				inner[fc.Column] = goqu.Op{"eq": fc.Value}
-			}
-		}
-		exprs = append(exprs, inner)
+	`)
+	if err != nil {
+		panic(err)
 	}
 
-	pp.Println(goqu.Select("c1").Where(exprs...).ToSQL())
+	// Create trigger
+	_, err = db.Exec(`
 
+		CREATE TRIGGER tablexyz_trigger_insert
+			BEFORE INSERT ON tablexyz
+		BEGIN
+		
+
+
+			INSERT INTO tablexyz_log(row_id, mod_sig, type, row_version, data)
+				VALUES (NEW.__id, '', 'INSERT', NEW.__version, NEW.data);
+		END;
+
+		CREATE TRIGGER tablexyz_trigger_update
+			BEFORE UPDATE ON tablexyz
+		BEGIN
+			INSERT INTO tablexyz_log(row_id, mod_sig, type, row_version, data)
+				VALUES (NEW.__id, '', 'UPDATE', NEW.__version, NEW.data);
+		END;
+
+
+
+
+	`)
+	if err != nil {
+		panic(err)
+	}
+
+	// Insert test data
+	_, err = db.Exec(`
+		INSERT INTO tablexyz (__id, __mod_sig, __version, data)
+		VALUES (1, '', 0, 'testxyzyzyyzyzyzy')
+	`)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Test harness completed successfully")
 }
