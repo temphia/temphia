f5b35dac57b971add18bcc330c33366fc6dab891
bornjre <bornjre@gmail.com>
2023-04-16 23:51:02 +0545

    set service context

--- code/frontend/entries/executor/pagequery/index.svelte
+++ code/frontend/entries/executor/pagequery/index.svelte
@@ -1,41 +1,63 @@
 <script lang="ts">
   import Tailwind from "./../../xcompo/common/_tailwind.svelte";
+  import DialogmodalCompo from "../../xcompo/dialogmodal/dialogmodalcompo.svelte";
 
   import Start from "./page/start/start.svelte";
   import Final from "./page/final/final.svelte";
   import Layout from "./page/_layout.svelte";
 
   import { LoadingSpinner } from "../../xcompo";
+  import { PageQueryService, KEY } from "./service";
+  import { setContext } from "svelte";
 
   export let env: any;
 
+  const service = new PageQueryService(env);
+
   let mode: "START" | "END" = "START";
   let loading = false;
-
+  let root_elem;
+  let modal;
   let data = {};
+
+  const next = (_data) => {
+    data = _data;
+    mode = "END";
+  };
+
+  const submit = async () => {
+    return {
+      ok: true,
+      data: {},
+    };
+  };
+
+  const load = async () => {};
+
+  setContext(KEY, {
+    get_service: () => service,
+    get_modal: () => modal,
+    get_root: () => ({
+      elem: root_elem,
+      load,
+    }),
+  });
 </script>
 
-{#if loading}
-  <LoadingSpinner />
-{:else if mode == "START"}
-  <Layout>
-    <Start
-      onNext={(_data) => {
-        data = _data;
-        mode = "END";
-      }}
-      onSubmit={async () => {
-        return {
-          ok: true,
-          data: {},
-        };
-      }}
-    />
-  </Layout>
-{:else}
-  <Layout>
-    <Final />
-  </Layout>
-{/if}
+<DialogmodalCompo bind:modal />
+
+<div bind:this={root_elem}>
+  {#if loading}
+    <LoadingSpinner />
+  {:else if mode == "START"}
+    <Layout>
+      <Start onNext={next} onSubmit={submit} />
+    </Layout>
+  {:else}
+    <Layout>
+      <Final />
+    </Layout>
+  {/if}
+</div>
 
 <Tailwind />
