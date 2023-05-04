1d97b539cd616aed3f5a07de3d20f62b56aaf8dc
bornjre <bornjre@gmail.com>
2023-05-05 03:44:49 +0545

    nit

--- code/frontend/scripts/buildpackage/build_npm_package.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const { execSync } = require("child_process");
-
-execSync("cp -f scripts/buildpackage/package.json package.json")
-
-execSync("tsc -p scripts/tsconfig.package.json")
