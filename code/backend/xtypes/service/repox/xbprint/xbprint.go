5ee19caeb2fae0e667ac66208cdd4001c94d5f00
bornjre <bornjre@gmail.com>
2023-05-26 03:34:03 +0545

    nit

--- code/backend/xtypes/service/repox/xbprint/xbprint.go
+++ code/backend/xtypes/service/repox/xbprint/xbprint.go
@@ -3,7 +3,6 @@ package xbprint
 const (
 	TypeBundle    = "bundle"
 	TypeDataGroup = "data_group"
-	TypeDataTable = "data_table"
 	TypeDataSheet = "data_sheet"
 	TypePlug      = "plug"
 	TypeResource  = "resource"
