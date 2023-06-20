f5dbff4471378512c16a2c16afe3e4f0d9ec4f81
bornjre <bornjre@gmail.com>
2023-06-21 04:18:28 +0545

    new goja bindings wip

--- code/backend/engine/executors/javascript1/goja2/instance.go
+++ code/backend/engine/executors/javascript1/goja2/instance.go
@@ -3,6 +3,7 @@ package evgoja
 import (
 	"github.com/dop251/goja"
 	"github.com/dop251/goja_nodejs/eventloop"
+	"github.com/dop251/goja_nodejs/require"
 	"github.com/temphia/temphia/code/backend/xtypes/etypes/bindx"
 )
 
@@ -17,23 +18,29 @@ type GojaInstance struct {
 }
 
 func NewInstance(tenantId string) *GojaInstance {
+	registry := require.NewRegistry()
 
-	evl := eventloop.NewEventLoop()
-	var rt *goja.Runtime
-
-	evl.Run(func(r *goja.Runtime) {
-		rt = r
-	})
-
-	return &GojaInstance{
+	gi := &GojaInstance{
 		tenantId: tenantId,
-		evLoop:   evl,
-		rt:       rt,
+		evLoop:   nil,
+		rt:       nil,
 
 		activeEvents: make(map[string]bindx.Bindings),
 		lastBinding:  nil,
 		needsClose:   false,
 	}
+
+	evl := eventloop.NewEventLoop()
+
+	evl.Run(func(r *goja.Runtime) {
+		gi.rt = r
+		gi.evLoop = evl
+
+		registry.Enable(r)
+		registry.RegisterNativeModule("temphia", gi.temphiaBindings)
+	})
+
+	return gi
 }
 
 func (g *GojaInstance) Init(b bindx.Bindings) error {
