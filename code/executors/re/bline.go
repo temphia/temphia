a3c8c51b05bed99c929c4ab5c21915ed13f3293d
bornjre <bornjre@gmail.com>
2023-07-11 19:22:28 +0545

    ctx and root bind call impl and refactor

--- code/executors/re/bline.go
+++ code/executors/re/bline.go
@@ -1,12 +1,13 @@
 package re
 
 import (
+	"bufio"
 	"encoding/json"
 	"net"
 
+	"github.com/k0kubun/pp"
 	"github.com/temphia/temphia/code/backend/engine/runtime/modipc"
 	"github.com/temphia/temphia/code/backend/libx/lazydata"
-	"github.com/temphia/temphia/code/executors/re/bind2ipc"
 	"github.com/temphia/temphia/code/executors/re/rtypes"
 )
 
@@ -25,7 +26,8 @@ type bindingsLine struct {
 		data  []byte
 	}
 
-	bipc bind2ipc.Bind2IPC
+	ctxipc  *modipc.ModIPC // 		bipc = modipc.NewModIPC(bind2ipc.New(c.runner.rootBinding))
+	rootipc *modipc.ModIPC
 }
 
 func (c *bindingsLine) run() {
@@ -36,7 +38,8 @@ func (c *bindingsLine) run() {
 }
 
 func (c *bindingsLine) readLoop() {
-	mipc := modipc.NewModIPC(&c.bipc)
+
+	reader := bufio.NewReader(c.conn)
 
 	for {
 
@@ -44,51 +47,74 @@ func (c *bindingsLine) readLoop() {
 			return
 		}
 
-		decoder := json.NewDecoder(c.conn)
-		packet := &rtypes.Packet{}
-
-		err := decoder.Decode(packet)
+		out, err := reader.ReadBytes('\n')
 		if err != nil {
-			c.closed = true
-			close(c.writeChan)
+			pp.Println("@read_error", err.Error())
+			continue
+		}
+
+		go c.handle(out)
+	}
+
+}
+
+func (c *bindingsLine) handle(data []byte) {
+
+	packet := &rtypes.Packet{}
+
+	err := json.Unmarshal(data, packet)
+	if err != nil {
+		c.closed = true
+		close(c.writeChan)
+		return
+	}
+
+	sendErr := func(err error) {
+		c.writeChan <- struct {
+			isErr bool
+			id    string
+			data  []byte
+		}{
+			isErr: true,
+			data:  []byte(err.Error()),
+		}
+	}
+
+	sendOk := func(out []byte) {
+		c.writeChan <- struct {
+			isErr bool
+			id    string
+			data  []byte
+		}{
+			isErr: false,
+			data:  out,
+		}
+
+	}
+
+	bipc := c.ctxipc
+
+	switch packet.Type {
+	case rtypes.ROOT_BINDING_CALL:
+		bipc = c.rootipc
+		fallthrough
+
+	case rtypes.CTX_BINDING_CALL:
+		resp, err := bipc.Handle(packet.Type, lazydata.NewJsonData(packet.Data))
+		if err != nil {
+			sendErr(err)
 			return
 		}
 
-		go func() {
+		out, err := resp.AsJsonBytes()
+		if err != nil {
+			sendErr(err)
+			return
+		}
+		sendOk(out)
 
-			senErr := func(err error) {
-				c.writeChan <- struct {
-					isErr bool
-					id    string
-					data  []byte
-				}{
-					isErr: true,
-					data:  []byte(err.Error()),
-				}
-			}
-
-			resp, err := mipc.Handle(packet.Type, lazydata.NewJsonData(packet.Data))
-			if err != nil {
-				senErr(err)
-				return
-			}
-
-			out, err := resp.AsJsonBytes()
-			if err != nil {
-				senErr(err)
-				return
-			}
-
-			c.writeChan <- struct {
-				isErr bool
-				id    string
-				data  []byte
-			}{
-				isErr: false,
-				data:  out,
-			}
-		}()
-
+	default:
+		pp.Println("@bline_not_impl_packet", packet)
 	}
 
 }
--- code/executors/re/rtypes/proto.go
+++ code/executors/re/rtypes/proto.go
@@ -1,7 +1,8 @@
 package rtypes
 
 const (
-	EVENT_PROCESS  = "event_process"
-	EVENT_RESPONSE = "event_response"
-	BINDING_CALL   = "bindings_call"
+	EVENT_PROCESS     = "event_process"
+	EVENT_RESPONSE    = "event_response"
+	CTX_BINDING_CALL  = "ctx_bindings_call"
+	ROOT_BINDING_CALL = "root_bindings_call"
 )
