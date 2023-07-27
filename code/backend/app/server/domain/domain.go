79ac6d28b10e60885930194622a68cb23f26ef89
bornjre <bornjre@gmail.com>
2023-07-28 03:04:58 +0545

    more placeholder stuff

---
+++ code/backend/app/server/domain/domain.go
@@ -0,0 +1,5 @@
+package domain
+
+type DomainAdapter struct {
+	RendererType string // agent_server, <custom>
+}
--- code/backend/app/server/server.go
+++ code/backend/app/server/server.go
@@ -1,1 +1,23 @@
 package server
+
+import "net/http"
+
+type Options struct {
+	RootDomain   string
+	RunnerDomain string
+	Port         string
+}
+
+type Server struct {
+	opts Options
+}
+
+func New(opts Options) *Server {
+	return &Server{
+		opts: opts,
+	}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+}
