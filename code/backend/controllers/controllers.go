fc448f30c415afd714c69d68775fa160ded22ef4
bornjre <bornjre@gmail.com>
2023-07-29 14:27:36 +0545

    remove operator stuff

--- code/backend/controllers/controllers.go
+++ code/backend/controllers/controllers.go
@@ -8,7 +8,6 @@ import (
 	"github.com/temphia/temphia/code/backend/controllers/data"
 	"github.com/temphia/temphia/code/backend/controllers/dev"
 	"github.com/temphia/temphia/code/backend/controllers/engine"
-	"github.com/temphia/temphia/code/backend/controllers/operator"
 	"github.com/temphia/temphia/code/backend/controllers/repo"
 	"github.com/temphia/temphia/code/backend/controllers/sockd"
 	"github.com/temphia/temphia/code/backend/controllers/tickets"
@@ -31,18 +30,17 @@ import (
 // to finish the operations [ api/routes => controller => services => db/providers]
 
 type RootController struct {
-	cAdmin    *admin.Controller
-	cAuth     *authed.Controller
-	cBasic    *basic.Controller
-	cCabinet  *cabinet.Controller
-	cDtable   *data.Controller
-	cDev      *dev.Controller
-	cEngine   *engine.Controller
-	cRepo     *repo.Controller
-	cSockd    *sockd.Controller
-	cOperator *operator.Controller
-	cUser     *user.Controller
-	cTicket   *tickets.Controller
+	cAdmin   *admin.Controller
+	cAuth    *authed.Controller
+	cBasic   *basic.Controller
+	cCabinet *cabinet.Controller
+	cDtable  *data.Controller
+	cDev     *dev.Controller
+	cEngine  *engine.Controller
+	cRepo    *repo.Controller
+	cSockd   *sockd.Controller
+	cUser    *user.Controller
+	cTicket  *tickets.Controller
 }
 
 type Options struct {
@@ -74,26 +72,19 @@ func New(opts Options) *RootController {
 		cCabinet: cabinet.New(cab, signer),
 		cDtable:  data.New(dynhub, pacman, cab, signer),
 		cRepo:    repo.New(pacman),
-		cOperator: operator.New(
-			corehub,
-			signer,
-			opts.App,
-			opts.OperatorUser,
-			opts.OperatorPassword),
-		cDev:    dev.New(pacman, corehub),
-		cEngine: engine.New(ehub, corehub),
-		cUser:   user.New(corehub),
-		cSockd:  sockd.New(deps.SockdHub().(sockdx.Hub).GetSockd()),
-		cTicket: tickets.New(corehub, pstate),
+		cDev:     dev.New(pacman, corehub),
+		cEngine:  engine.New(ehub, corehub),
+		cUser:    user.New(corehub),
+		cSockd:   sockd.New(deps.SockdHub().(sockdx.Hub).GetSockd()),
+		cTicket:  tickets.New(corehub, pstate),
 	}
 }
 
-func (c *RootController) AdminController() *admin.Controller       { return c.cAdmin }
-func (c *RootController) AuthController() *authed.Controller       { return c.cAuth }
-func (c *RootController) BasicController() *basic.Controller       { return c.cBasic }
-func (c *RootController) CabinetController() *cabinet.Controller   { return c.cCabinet }
-func (c *RootController) DtableController() *data.Controller       { return c.cDtable }
-func (c *RootController) OperatorController() *operator.Controller { return c.cOperator }
+func (c *RootController) AdminController() *admin.Controller     { return c.cAdmin }
+func (c *RootController) AuthController() *authed.Controller     { return c.cAuth }
+func (c *RootController) BasicController() *basic.Controller     { return c.cBasic }
+func (c *RootController) CabinetController() *cabinet.Controller { return c.cCabinet }
+func (c *RootController) DtableController() *data.Controller     { return c.cDtable }
 
 func (c *RootController) SockdController() *sockd.Controller    { return c.cSockd }
 func (c *RootController) RepoController() *repo.Controller      { return c.cRepo }
