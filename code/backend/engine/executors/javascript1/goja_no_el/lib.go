27292fdca043cd8c98a2a4146108d217ad11ce79
bornjre <bornjre@gmail.com>
2023-07-29 14:28:02 +0545

    fix

--- code/backend/engine/executors/javascript1/goja_no_el/lib.go
+++ code/backend/engine/executors/javascript1/goja_no_el/lib.go
@@ -1,14 +1,18 @@
 package goja
 
 import (
+	_ "embed"
+
 	"github.com/dop251/goja"
-	sdkdist "github.com/temphia/temphia/code/backend/engine/executors/javascript1/sdk/dist"
 )
 
 var libesplug *goja.Program
 
+//go:embed libesplug.js
+var libesPlug string
+
 func init() {
-	program, err := goja.Compile("libesplug", sdkdist.LibESPlug, true)
+	program, err := goja.Compile("libesplug", libesPlug, true)
 	if err != nil {
 		panic(err)
 	}
