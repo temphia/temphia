c795232134ec0a93b6082831447de7377aca5fe9
bornjre <bornjre@gmail.com>
2023-08-01 15:38:26 +0545

    multiple entry config

--- code/frontend/ui/vite.config.ts
+++ code/frontend/ui/vite.config.ts
@@ -1,16 +1,36 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig, UserConfig } from 'vite'
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+const sp = svelte({
+  preprocess: vitePreprocess(),
+})
+
+const isAuth = process.env.AUTH_UI
+
+let config: UserConfig = {
+  plugins: [sp],
   build: {
     outDir: "dist",
     lib: {
-      entry: ["src/main.ts"],
+      entry: ["src/entries/portal/index.ts"],
       formats: ["iife"],
-      name: "mmmm"
+      name: "portal"
     },
-    
   },
-})
+}
+
+if (isAuth) {
+  config = {
+    plugins: [sp],
+    build: {
+      outDir: "dist",
+      lib: {
+        entry: ["src/entries/auth/index.ts"],
+        formats: ["iife"],
+        name: "auth"
+      },
+    },
+  }
+}
+
+export default defineConfig(config)
