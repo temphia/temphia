9e189f0c493e80ff5d34df8d4f0c6ee35dfce3b9
bornjre <bornjre@gmail.com>
2023-08-01 14:46:17 +0545

    stash new frontend js

---
+++ code/frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: ["src/main.ts"],
+      formats: ["iife"],
+      name: "mmmm"
+    },
+    
+  },
+})
