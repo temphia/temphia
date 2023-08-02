3ad3bab58afbb397366b73a6013231d6b83e34b0
bornjre <bornjre@gmail.com>
2023-08-03 01:06:48 +0545

    utils function

--- code/backend/libx/xutils/os.go
+++ code/backend/libx/xutils/os.go
@@ -3,6 +3,7 @@ package xutils
 import (
 	"errors"
 	"io"
+	"net"
 	"os"
 	"path"
 
@@ -53,4 +54,18 @@ func Copy(srcFile, dstFile string) error {
 	}
 
 	return nil
+}
+
+func GetFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
 }
