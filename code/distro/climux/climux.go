3ad8e6e9493d4fc45fbf11350063b80e3b3bfc85
bornjre <bornjre@gmail.com>
2023-08-03 01:00:58 +0545

    add climuxer

---
+++ code/distro/climux/climux.go
@@ -0,0 +1,16 @@
+package climux
+
+import "sync"
+
+type CLiFunc func(args ...string) error
+
+var (
+	cliRegistery = map[string]CLiFunc{}
+	cLock        sync.Mutex
+)
+
+func Register(name string, f CLiFunc) {
+	cLock.Lock()
+	cliRegistery[name] = f
+	cLock.Unlock()
+}
