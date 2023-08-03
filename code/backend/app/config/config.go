da1a25982607fc2a85f563395cb40d5b696df62e
bornjre <bornjre@gmail.com>
2023-08-03 15:37:07 +0545

    add log injest config

--- code/backend/app/config/config.go
+++ code/backend/app/config/config.go
@@ -1,16 +1,17 @@
 package config
 
 type Config struct {
-	ServerPort      string       `json:"server_port,omitempty"`
-	TenantId        string       `json:"tenant_id,omitempty"`
-	RootDomain      string       `json:"root_domain,omitempty"`
-	RunnerDomain    string       `json:"runner_domain,omitempty"`
-	MasterKey       string       `json:"master_key,omitempty"`
-	EnableLocalDoor bool         `json:"enable_local_door,omitempty"`
-	DataFolder      string       `json:"data_folder,omitempty"`
-	LogFolder       string       `json:"log_folder,omitempty"`
-	DatabaseConfig  *StoreConfig `json:"database_config,omitempty"`
-	FileStoreConfig *StoreConfig `json:"filestore_config,omitempty"`
+	ServerPort      string        `json:"server_port,omitempty"`
+	TenantId        string        `json:"tenant_id,omitempty"`
+	RootDomain      string        `json:"root_domain,omitempty"`
+	RunnerDomain    string        `json:"runner_domain,omitempty"`
+	MasterKey       string        `json:"master_key,omitempty"`
+	EnableLocalDoor bool          `json:"enable_local_door,omitempty"`
+	DataFolder      string        `json:"data_folder,omitempty"`
+	LogFolder       string        `json:"log_folder,omitempty"`
+	DatabaseConfig  *StoreConfig  `json:"database_config,omitempty"`
+	FileStoreConfig *StoreConfig  `json:"filestore_config,omitempty"`
+	LogIngestConfig *IngestConfig `json:"log_ingest_config,omitempty"`
 }
 
 type StoreConfig struct {
@@ -23,4 +24,10 @@ type StoreConfig struct {
 	Port     string         `json:"port,omitempty"`
 	Options  map[string]any `json:"options,omitempty"`
 	Target   string         `json:"target,omitempty"`
+}
+
+type IngestConfig struct {
+	Upstream string `json:"upstream,omitempty"`
+	User     string `json:"user,omitempty"`
+	Password string `json:"password,omitempty"`
 }
