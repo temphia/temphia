af8fa0985891d3636a9d76eb508c38331b2762cf
bornjre <bornjre@gmail.com>
2023-08-27 05:45:23 +0545

    login page placeholder

--- code/frontend2/src/routes/auth/login/+page.svelte
+++ code/frontend2/src/routes/auth/login/+page.svelte
@@ -1,1 +1,15 @@
-<div>Login</div>
+<div> 
+    <h3 class="h3">User Login</h3>
+
+    <label class="label my-1">
+        <span>User</span>
+        <input class="input p-2" type="text" placeholder="User1" />
+    </label>
+
+    <label class="label my-1">
+        <span>Password</span>
+        <input class="input p-2" title="Password" type="password" placeholder="password" />
+    </label>
+
+    <button type="button" class="btn variant-filled my-1">Login</button>
+</div>
