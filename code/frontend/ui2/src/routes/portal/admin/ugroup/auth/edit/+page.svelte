5178e522679c355a2f5d81115930f6bfc538b53c
bornjre <bornjre@gmail.com>
2023-09-05 12:51:18 +0545

    ugroup auth page back

---
+++ code/frontend/ui2/src/routes/portal/admin/ugroup/auth/edit/+page.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+  import { AutoForm, LoadingSpinner, PortalService } from "$lib/core";
+  import { params } from "$lib/params";
+
+  export let ugroup = $params["ugroup"];
+  export let id = $params["id"];
+
+  const app = getContext("__app__") as PortalService;
+  const api = app.api_manager.get_admin_ugroup_api();
+
+  let message = "";
+  let data = {};
+  let loading = true;
+
+  const load = async () => {
+    loading = true
+    const resp = await api.getAuth(ugroup, id);
+    if (!resp.ok) {
+      return;
+    }
+    data = resp.data;
+    loading = false
+
+  };
+
+  load();
+
+  const save = async (_data) => {
+    loading = true
+    const resp = await api.updateAuth(ugroup, id, _data);
+    if (!resp.ok) {
+      message = resp.data;
+      loading = false;
+      return;
+    }
+    app.nav.admin_ugroup_auths(ugroup);
+  };
+</script>
+
+{#if loading}
+  <LoadingSpinner />
+{:else}
+  <AutoForm
+    {message}
+    schema={{
+      fields: [
+        {
+          name: "Id",
+          ftype: "INT",
+          key_name: "id",
+          disabled: true,
+        },
+
+        {
+          name: "Name",
+          ftype: "TEXT",
+          key_name: "name",
+        },
+
+        {
+          name: "Type",
+          ftype: "TEXT",
+          key_name: "type",
+        },
+
+        {
+          name: "Provider",
+          ftype: "TEXT",
+          key_name: "provider",
+        },
+
+        {
+          name: "User Group",
+          ftype: "TEXT",
+          key_name: "user_group",
+          disabled: true,
+        },
+
+        {
+          name: "Scopes",
+          ftype: "MULTI_TEXT",
+          key_name: "scopes",
+        },
+
+        {
+          name: "New User If Not Exist",
+          ftype: "BOOL",
+          key_name: "newuser_ifnot_exists",
+        },
+
+        {
+          name: "Policy",
+          ftype: "TEXT_POLICY",
+          key_name: "policy",
+        },
+
+        {
+          name: "Extra Meta",
+          ftype: "KEY_VALUE_TEXT",
+          key_name: "extra_meta",
+        },
+      ],
+      name: "Edit User Group Auth",
+      required_fields: [],
+    }}
+    onSave={save}
+    {data}
+  />
+{/if}
