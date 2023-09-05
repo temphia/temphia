317fb27cc2b58a07f88d40d32402071cf1c817c1
bornjre <bornjre@gmail.com>
2023-09-05 20:45:56 +0545

    extension and add resource agent add

---
+++ code/frontend/ui2/src/routes/portal/admin/plug/agent/resource/+page.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+  import {
+    AutoTable,
+    LoadingSpinner,
+    FloatingAdd,
+    PortalService,
+  } from "../../../core";
+
+  import { params } from "svelte-hash-router";
+
+  const app = getContext("__app__") as PortalService;
+
+  let datas = [];
+  let loading = true;
+  const api = app.api_manager.get_admin_plug_api();
+
+  const load = async () => {
+    const resp = await api.list_agent_resource($params.pid, $params.aid);
+    if (!resp.ok) {
+      return;
+    }
+
+    datas = resp.data;
+    loading = false;
+  };
+
+  load();
+
+  // actions
+
+  const action_edit = (id: string) => app.nav.admin_agent_res_edit($params.pid, $params.aid, id);
+  const action_delete = async (id: string) => {
+    const resp = await api.delete_agent_resource($params.pid, $params.aid, id);
+    if (!resp.ok) {
+      return;
+    }
+    load();
+  };
+
+  const action_new = () =>
+    app.nav.admin_agent_res_new($params.pid, $params.aid);
+</script>
+
+{#if loading}
+  <LoadingSpinner />
+{:else}
+  <AutoTable
+    action_key="slug"
+    actions={[
+      {
+        Name: "Edit",
+        Action: action_edit,
+        icon: "pencil-alt",
+      },
+      {
+        Name: "Delete",
+        Class: "bg-red-400",
+        Action: action_delete,
+        icon: "trash",
+      },
+    ]}
+    key_names={[
+      ["slug", "Slug"],
+      ["resource_id", "Resource Id"],
+      ["plug_id", "Plug Id"],
+      ["agent_id", "Agent Id"],
+    ]}
+    color={["type"]}
+    {datas}
+  />
+{/if}
+
+<FloatingAdd onClick={action_new} />
---
+++ code/frontend/ui2/src/routes/portal/admin/plug/agent/resource/edit/+page.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+  import { AutoForm, LoadingSpinner, PortalService } from "$lib/core";
+  import { params } from "$lib/params";
+
+  const app = getContext("__app__") as PortalService;
+  const api = app.api_manager.get_admin_plug_api();
+
+  let message = "";
+  let data = {};
+  let loading = true;
+
+  const load = async () => {
+    const resp = await api.get_agent_resource(
+      $params["pid"],
+      $params["aid"],
+      $params["rid"]
+    );
+    if (!resp.ok) {
+      return;
+    }
+
+    data = resp.data;
+    loading = false;
+  };
+
+  const save = async (_data) => {
+    const resp = await api.update_agent_resource(
+      $params["pid"],
+      $params["aid"],
+      $params["rid"],
+      _data
+    );
+    if (!resp.ok) {
+      message = resp.data;
+      return;
+    }
+    app.nav.admin_agents($params["pid"]);
+  };
+
+  load();
+</script>
+
+{#if loading}
+  <LoadingSpinner />
+{:else}
+  <AutoForm
+    {message}
+    schema={{
+      fields: [
+        {
+          name: "Slug",
+          ftype: "TEXT_SLUG",
+          key_name: "slug",
+        },
+
+        {
+          name: "Policy",
+          ftype: "TEXT_POLICY",
+          key_name: "policy",
+        },
+
+        {
+          name: "Plug Id",
+          ftype: "TEXT",
+          key_name: "plug_id",
+        },
+
+        {
+          name: "Agent Id",
+          ftype: "TEXT",
+          key_name: "agent_id",
+        },
+
+        {
+          name: "Resource Id",
+          ftype: "TEXT",
+          key_name: "resource_id",
+        },
+
+        {
+          name: "Actions",
+          ftype: "MULTI_TEXT",
+          key_name: "actions",
+        },
+
+        {
+          name: "Extra Meta",
+          ftype: "KEY_VALUE_TEXT",
+          key_name: "extra_meta",
+        },
+      ],
+      name: "Edit Agent Resource",
+      required_fields: [],
+    }}
+    onSave={save}
+    {data}
+  />
+{/if}
