ca06fcafe2f888cdaff8091d86fe3801286195dd
bornjre <bornjre@gmail.com>
2023-10-06 15:43:36 +0545

    help text placeholder

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -1,14 +1,36 @@
 package cli
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/k0kubun/pp"
 	"github.com/temphia/temphia/code/distro"
 )
 
+const helpText = `
+Temphia is a platform for apps.
+
+Usage:
+
+        temphia <command> [arguments]
+
+The commands are:
+		{{ }}
+        version     print Temphia version
+        help        this help page
+
+Use "temphia <command> help" for more information about a command.
+
+
+`
+
 func Run() {
-	pp.Println("@i_am_cli")
+	if len(os.Args) > 1 {
+		if os.Args[1] == "help" || os.Args[1] == "--help" {
+			fmt.Print(helpText)
+			return
+		}
+	}
 
 	err := distro.RunAppCLI(os.Args)
 	if err != nil {
