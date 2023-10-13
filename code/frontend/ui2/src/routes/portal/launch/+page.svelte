ebbd32b666541b2cb590b0c4305c1fb81a0f2f94
bornjre <bornjre@gmail.com>
2023-10-14 03:42:42 +0545

    fix bug empty target instance

--- code/frontend/ui2/src/routes/portal/launch/+page.svelte
+++ code/frontend/ui2/src/routes/portal/launch/+page.svelte
@@ -10,19 +10,25 @@
     onMount(() => {
         let lopts = app.nav.options || {};
 
-        let instance = launcher.target_index[$params["target"]];
-        if (instance) {
-            launcher.instance_change(instance);
-        } else {
-            const name = $params["name"] ? window.atob($params["name"]) : "";
-            instance = launcher.instance_by_target({
-                invoker_name: "user_app",
-                target_id: $params["target"],
-                target_name: name,
-                target_type: lopts["target_type"] || "",
-                startup_payload: {},
-                invoker: null,
-            });
+        const target = $params["target"];
+
+        if (target) {
+            let instance = launcher.target_index[target];
+            if (instance) {
+                launcher.instance_change(instance);
+            } else {
+                const name = $params["name"]
+                    ? window.atob($params["name"])
+                    : "";
+                instance = launcher.instance_by_target({
+                    invoker_name: "user_app",
+                    target_id: target,
+                    target_name: name,
+                    target_type: lopts["target_type"] || "",
+                    startup_payload: {},
+                    invoker: null,
+                });
+            }
         }
 
         launcher.plane_show();
