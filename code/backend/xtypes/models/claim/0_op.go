968243213a700f52f4579fdf8db75bc38afe8907
bornjre <bornjre@gmail.com>
2023-10-15 15:18:56 +0545

    add fixme and add tenant field to lsock

--- code/backend/xtypes/models/claim/0_op.go
+++ code/backend/xtypes/models/claim/0_op.go
@@ -6,10 +6,13 @@ type Operator struct {
 	BindDeviceId string `json:"bind_device,omitempty"`
 }
 
+// fixme => change lsock to remote executor branding and move lsock to engine api ??
+
 type LSock struct {
-	Type  string `json:"type,omitempty"`
-	IID   int64  `json:"iid,omitempty"` // instance id
-	EID   int64  `json:"eid,omitempty"` // execution id
-	Plug  string `json:"plug,omitempty"`
-	Agent string `json:"agent,omitempty"`
+	TenantId string `json:"tenant_id,omitempty"`
+	Type     string `json:"type,omitempty"`
+	IID      int64  `json:"iid,omitempty"` // instance id
+	EID      int64  `json:"eid,omitempty"` // execution id
+	Plug     string `json:"plug,omitempty"`
+	Agent    string `json:"agent,omitempty"`
 }
