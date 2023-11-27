07603f3e38a79190c2370a0e3c183f79dfaa85b6
bornjre <bornjre@gmail.com>
2023-11-27 11:53:08 +0545

    generated frontend code

---
+++ code/frontend/ui2/build/_app/immutable/chunks/paths.3df37c61.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1te9dq2)==null?void 0:s.base)??"/z/pages";var e;const a=((e=globalThis.__sveltekit_1te9dq2)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ code/frontend/ui2/build/_app/immutable/chunks/stores.8bebdcaa.js
@@ -0,0 +1,1 @@
+import{d as r}from"./singletons.044f9e02.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},a={subscribe(s){return e().page.subscribe(s)}},i={subscribe(s){return e().navigating.subscribe(s)}};export{i as n,a as p};
